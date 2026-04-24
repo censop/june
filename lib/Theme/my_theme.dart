cd744e31021faa41180ac547b0d7e43951591669
Cansu Dogan <cansuiris@gmail.com>
2026-04-25 00:21:33 +0200

    inter font & placeholder homepage implemented

---
+++ lib/Theme/my_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+
+  static const String interFont = "Inter";
+
+  //static final TextTheme _baseTextTheme = TextTheme(); //will fill it when ui is finalized
+
+  static ThemeData lightTheme = ThemeData(
+    fontFamily: interFont
+  );
+}
